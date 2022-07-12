954f46b7cd334eedb0bb715950e044e4c516a75f
yashvi1713 <yashvishukla1713@gmail.com>
2022-07-12 19:29:51 +0530

    Added result display on search

--- script.js
+++ script.js
@@ -6,20 +6,48 @@ SearchBtn.addEventListener("click" , () => {
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     console.log(finalURL);
     fetch(finalURL)
-       .then((response) => response.json()) //.then((hey) => hey.json()) can also be done likethis,responseisjustname
+       .then((response) => response.json())
        .then((data) => {console.log(data[0]);
-       console.log(data[0].capital[0]);
-       console.log(data[0].flags.svg);
-       console.log(data[0].name.common);
-       console.log(Object.keys(data[0].currencies)[0]);
-       console.log(data[0].currencies[Object.keys(data[0].currencies)]);
-       console.log(data[0].languages[Object.keys(data[0].languages)[0]]);
-       console.log(data[0].maps.googleMaps);
+        console.log(data[0].capital[0]);
+        console.log(data[0].continents[0]);
+        console.log(data[0].flags.svg);
+        console.log(data[0].name.common);
+        console.log(Object.keys(data[0].currencies)[0]);
+        console.log(data[0].currencies[Object.keys(data[0].currencies)]);
+        console.log(data[0].languages[Object.keys(data[0].languages)[0]]);
+        console.log(data[0].maps.googleMaps);
+       document.getElementById("result").innerHTML = `<img src="${data[0].flags.svg}" class="flag-img">
+       <h2>${data[0].name.common}</h2>
+       <div class="wrapper">
+          <div class="data-wrapper">
+              <h3>Capital:</h3>
+              <span>${data[0].capital[0]}</span>
+          </div>
+          <div class="data-wrapper">
+              <h3>Continent:</h3>
+              <span>${data[0].continents[0]}</span>
+          </div>
+          <div class="data-wrapper">
+              <h3>Population:</h3>
+              <span>${data[0].population}</span>
+          </div>
+          <div class="data-wrapper">
+              <h3>Currency:</h3>
+              <span>${Object.keys(data[0].currencies)[0]} - ${data[0].currencies[Object.keys(data[0].currencies)].name}</span>
+          </div>
+          <div class="data-wrapper">
+          <h3>Common Languages:</h3>
+          <span>${Object.values(data[0].languages).toString().split(",").join(", ")}</span>
+      </div>
+       </div>`
+    })
+    .catch(() => {
+        if(countryName.length == 0){
+            document.getElementById("result").innerHTML=`<h4>The input field cannot be empty</h4>`;
+        }
+        else{
+            document.getElementById("result").innerHTML='<h4>Please enter a valid country name</h4>';
+        }
     })
 });
  
-//EXPLANATION OF LINE 14
-//currencies ni ander IND ni value joi ti toh Objects.keys() method thi array of string ma convert kary ane array ni first value etle index 0 ma IND value lidhi {IND ni jagya a je country ni cuurency name hoy a}
-
- // 
- //currencies ek array che jeni ander object che INR have eni ander key name ni value levi che so for that savathi pehla currencies ni array ma Object.keys() method use karine ene array of string ma convert karine name vali key ne call kari ane value lidhi  {IND ni jagya a je country ni cuurency name hoy a}
